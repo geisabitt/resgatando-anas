4226d34a0da639a0219283c1e440f56a09d001ed
geisabitt <ageisislane@gmail.com>
2024-02-01 11:36:07 -0300

    add development space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
       <div><Header/></div>
       <div><Information/></div>
       <div  className="flex flex-col items-center justify-center min-w-[340px] max-w-[500px] my-4 mx-auto gap-4">
-        <p>OBS.: Essa forma de pagamento não tem como customizar</p>
         <Link className="p-4 flex" href="/retiro/pagamento"><Button className="bg-[#ffa621] hover:bg-[#996c28] rounded-[10px] px-24 py-4">Pagamento</Button></Link>
       </div>
     </main>
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import AuthService from './auth/service/auth-service';
 
 export const config = {
-  matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
+  matcher: '/((?!_next/static|_next/image|favicon.ico|_next/img|foto-header.png).*)',
 };
 
 const publicRoutes = ['/',
@@ -10,14 +10,17 @@ const publicRoutes = ['/',
                       '/retiro/login',
                       '/retiro/pagamento',
                       '/retiro/statusOk',
-                      '/retiro/statusBad'];
+                      '/retiro/statusBad'
+                      ];
+
+const publicFolders = ['/img']; // Adicione a pasta 'img' aos recursos públicos
 
 export async function middleware(req: NextRequest) {
   console.log(req.nextUrl);
 
   const pathname = req.nextUrl.pathname;
 
-  if (publicRoutes.includes(pathname)) {
+  if (publicRoutes.includes(pathname) || publicFolders.some(folder => pathname.startsWith(folder))) {
     return NextResponse.next();
   }
 
