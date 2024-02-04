74ac444d851106dad040f74b6b2bd946eff957ae
geisabitt <ageisislane@gmail.com>
2024-02-04 13:12:12 -0300

    update: page retiro index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
       <div><HamburguerMenu/></div>
       <div><Header/></div>
       <div><Information/></div>
-      <div  className="flex flex-col items-center justify-center min-w-[340px] max-w-[500px] my-4 mx-auto gap-4">
-        <Link className="p-1 flex" href="/retiro/pagamento"><Button className="bg-[#ffa621] hover:bg-[#996c28] rounded-[10px] px-24 py-4">Pagamento</Button></Link>
-        <Link className="p-1 flex" href="/retiro/pagamentoPix"><Button className="bg-[#ffa621] hover:bg-[#996c28] rounded-[10px] px-24 py-4">Pagamento Pix</Button></Link>
+      <div  className="flex flex-col items-center justify-center min-w-[340px] max-w-[500px] my-4 mx-auto gap-2">
+        <Link className="flex" href="/retiro/pagamento"><Button className="bg-[#ffa621] hover:bg-[#996c28] rounded-[10px] px-24 py-4">Pagamento</Button></Link>
+        {/* <Link className="flex" href="/retiro/pagamentoPix"><Button className="bg-[#ffa621] hover:bg-[#996c28] rounded-[10px] px-24 py-4">Pagamento Pix</Button></Link> */}
       </div>
     </main>
   )
