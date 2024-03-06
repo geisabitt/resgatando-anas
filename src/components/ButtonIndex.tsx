357225e8828067c6c0831d3d97249ee437e02d89
geisabitt <ageisislane@gmail.com>
2024-03-06 14:20:08 -0300

    feat: footer menu and ajust header menu

--- src/components/ButtonIndex.tsx
+++ src/components/ButtonIndex.tsx
@@ -2,8 +2,6 @@ import Link from "next/link";
 
 export default function ButtonIndex() {
     return (
-        <>
-            <Link className="px-4 py-2 rounded bg-accent fixed bottom-4 right-4" href="/">Inicio</Link>
-        </>
+        <Link className="px-4 py-2 rounded bg-accent fixed bottom-4 right-4" href="/">Inicio</Link>
     );
 }
