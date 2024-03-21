412efae7e956c886f17f79cf15874b35dd05632d
geisabitt <ageisislane@gmail.com>
2024-03-21 16:11:32 -0300

    refactor: item gallery skeleton

--- src/components/landingPage/galery/itemGallery.tsx
+++ src/components/landingPage/galery/itemGallery.tsx
@@ -1,29 +1,48 @@
 "use client"
 import Image from "next/image";
 import { Skeleton } from "@/components/ui/skeleton";
-import { useState } from 'react';
+import { useState, useEffect  } from 'react';
 interface GalleryItemProps {
     imageUrl: string;
     onClick: () => void;
 }
 export default function ItemGallery({ imageUrl, onClick }: Readonly<GalleryItemProps>) {
   const [loading, setLoading] = useState(true);
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
-  const handleImageLoad = () => {
-    setLoading(false);
-  };
+  useEffect(() => {
+    const loadImage = async () => {
+      try {
+        const response = await fetch(imageUrl);
+        const blob = await response.blob();
+        const src = URL.createObjectURL(blob);
+        setImageSrc(src);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error loading image:', error);
+      }
+    };
+
+    loadImage();
+
+    return () => {
+      if (imageSrc) {
+        URL.revokeObjectURL(imageSrc);
+      }
+    };
+  }, [imageUrl]);
+
     return (
       <div onClick={onClick} className="w-24 h-24 overflow-hidden rounded-lg">
       {loading ? (
         <Skeleton className="h-[92px] w-[92px] rounded-xl"  />
       ) : (
         <Image
-          className="rounded-[10px]"
+          className="rounded-[10px] w-full h-full object-cover"
           width={92}
           height={92}
           src={imageUrl}
           alt="Gallery Item"
-          onLoad={handleImageLoad}
         />
       )}
     </div>
