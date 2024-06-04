cc42c525e0607fa58948869722391bf1408c6e24
geisabitt <ageisislane@gmail.com>
2024-06-04 19:24:59 -0300

    install cookie

--- src/app/retiro/login/page.tsx
+++ src/app/retiro/login/page.tsx
@@ -1,9 +1,37 @@
-import { LoginForm } from "@/auth/components/loginForm";
+import * as React from "react"
 
-export default function Login() {
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+import { AlertSistem } from "@/components/shared/alertSistem"
+
+export function Page() {
   return (
-    <div className="flex align-center justify-center p-20">
-        <LoginForm />
-    </div>
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Preencha seu dados para logar.</CardDescription>
+      </CardHeader>
+        <form>
+        <CardContent>
+            <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">E-mail</Label>
+                <Input name='email' type='email' id="email" placeholder="Digite seu email" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Senha</Label>
+                <Input name='password' type='password' id="password" placeholder="Digite uma senha segura" />
+                </div>
+            </div>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+            <Button type='submit'>Entrar</Button>
+            <Link href='/retiro/cadastro' className={buttonVariants({variant:"outline"})}>Não tenho cadastro</Link>
+        </CardFooter>
+        </form>
+    </Card>
   )
 }
