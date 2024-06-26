9855b607ec222019ab34186e3414e9196e961186
geisabitt <ageisislane@gmail.com>
2024-06-26 19:40:21 -0300

    fix: ajuste nos dominio da imagem

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["localhost", "resgatando-anas.vercel.app"],
+        domains: ["localhost", "resgatando-anas.vercel.app", "resgatandoanasofc.vercel.app"],
       }
 }
 
