382f232ae19570c779e9dafdc602c8ac251d75da
geisabitt <ageisislane@gmail.com>
2024-07-09 12:45:13 -0300

    update: retorno dos dados para pagina administrativa

--- src/app/api/adm/get-user/[id]/route.ts
+++ src/app/api/adm/get-user/[id]/route.ts
@@ -1,4 +1,3 @@
-
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -7,41 +6,46 @@ const prisma = new PrismaClient();
 export async function GET(req: NextRequest) {
   const { pathname } = new URL(req.url);
   const id = pathname.split('/').pop();
+  console.log(id);
 
-    try {
+  try {
+    if (id) {
+      const user = await prisma.users.findFirst({
+          where: { id },
+          select: {
+              id: true,
+              email: true,
+              name: true,
+              telefone: true,
+              telefone_emergencia: true,
+              cpf: true,
+              rg:true,
+              data_de_nascimento: true,
+              type: true,
+          }
+      });
 
-      if (id) {
-        const user = await prisma.users.findFirst({
-            where: { id },
-            select: {
-                id: true,
-                email: true,
-                name: true,
-                telefone: true,
-                telefone_emergencia: true,
-                cpf: true,
-                rg:true,
-                data_de_nascimento: true,
-                type: true,
-            }
-        });
+      const payments = await prisma.paymentUser.findMany({
+          where: { userId: id },
+      });
 
-          const payments = await prisma.paymentUser.findMany({
-              where: { userId: id },
-          });
-          const anaminese = await prisma.usersAnaminese.findFirst({
-            where: { userId: id },
-        });
+      const anaminese = await prisma.usersAnaminese.findFirst({
+          where: { userId: id },
+      });
 
+      if (user && payments && anaminese) {
+        return NextResponse.json({ status: 200, user, payments, anaminese });
+      } else if (user && payments && !anaminese) {
+        return NextResponse.json({ status: 200, user, payments, error: 'Dados médicos não encontrados'});
+      } else if (user && !payments && !anaminese) {
+        return NextResponse.json({ status: 200, user, payments, error: 'Dados médicos e pagamentos não encontrados'});
+      }
 
-        if (user && payments && anaminese) {
-            return NextResponse.json({ status: 200, user, payments, anaminese });
-        }
     }
-    return NextResponse.json({ message: 'Usuario não encontrado', status: 400 });
+    return NextResponse.json({ message: 'Usuário não encontrado', status: 400 });
 
-    } catch (error) {
-        console.error("Ocorreu um erro:", error);
-        return NextResponse.json({ message: 'Ocorreu um erro ao processar a solicitação', status: 500 });
-    }
+  } catch (error) {
+      console.error("Ocorreu um erro:", error);
+      return NextResponse.json({ message: 'Ocorreu um erro ao processar a solicitação', status: 500 });
+  }
 }
