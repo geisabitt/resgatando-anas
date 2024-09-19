4b46311bb8e068feeef3e49b641e0cdf483e75df
geisabitt <ageisislane@gmail.com>
2024-09-19 16:09:41 -0300

    correção de texto do status do pagamento

--- src/app/user/components-local/user-card-payments.tsx
+++ src/app/user/components-local/user-card-payments.tsx
@@ -12,7 +12,7 @@ export default function UserCardPayments({ payment }: Readonly<{ payment: Displa
             <p>Identificador: {payment.paymentId}</p>
             <p>Forma de Pagamento: {payment.paymentType}</p>
             <p>Status: {payment.paymentStatus}</p>
-            <p>Data de Criação: {payment.createdAt ? formatDate(payment.createdAt) : ''}</p>
+            <p>Data da Compra: {payment.createdAt ? formatDate(payment.createdAt) : ''}</p>
             <p>Valor: {payment.paymentId !== '' ? 'R$250,00' : ''}</p>
             <Link className="w-full py-4 rounded text-center text-white bg-success700 mt-4" href={payment.url}>
                 {payment.btnText}
