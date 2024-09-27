12acbffc8efee33d29ea1beedf271a12062b8cda
geisabitt <ageisislane@gmail.com>
2024-09-27 18:26:32 -0300

    correção do deploy vercel2

--- src/payments/actions/paymentActions.ts
+++ src/payments/actions/paymentActions.ts
@@ -11,7 +11,7 @@ const prisma = new PrismaClient();
 async function createPaymentPix(){
     'use server';
 
-    const userId = String(await AuthService.getUserId());
+    const userId = String(await AuthService.creatRouteId());
     const user = await prisma.users.findFirst({
             where: { id: userId },
             select: {
